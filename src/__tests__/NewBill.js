0884be2cf3aa5e7ff42aded3169ee3df31954a59
nitatemic <alexandre@nitatemic.dev>
2024-07-02 16:14:53 +0200

    Better test for handleChangeFile

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -4,7 +4,7 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/dom';
+import { fireEvent, screen, waitFor } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import NewBillUI from '../views/NewBillUI.js';
@@ -25,10 +25,11 @@ describe('Given I am connected as an employee', () => {
         value: localStorageMock,
       });
       window.localStorage.setItem(
-        'user',
+        "user",
         JSON.stringify({
-          type: 'Employee',
-        }),
+          type: "Employee",
+          email: "employee@test.tdl",
+        })
       );
 
       const root = document.createElement('div');
@@ -37,6 +38,8 @@ describe('Given I am connected as an employee', () => {
 
       Router();
       window.onNavigate(ROUTES_PATH.NewBill);
+
+
     });
     afterEach(() => {
       localStorage.clear(); // Effacer les données de localStorage
@@ -78,23 +81,22 @@ describe('Given I am connected as an employee', () => {
     });
 
     it('Should test if we can send something else than the authorized files', async () => {
-      /* reset the html */
-      document.body.innerHTML = '';
-      const html = NewBillUI();
-      document.body.innerHTML = html;
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
       /* Define the local storage */
       Object.defineProperty(window, 'localStorage', {
         value: localStorageMock,
       });
       window.localStorage.setItem(
-        'user',
+        "user",
         JSON.stringify({
-          type: 'Employee',
-        }),
-      );
+          type: "Employee",
+          email: "employee@test.tdl",
+        })
+      )
+
+      document.body.innerHTML = NewBillUI();
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
 
       const newBill = new NewBill({
         document,
@@ -103,7 +105,7 @@ describe('Given I am connected as an employee', () => {
         localStorage: window.localStorage,
       });
 
-      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+      const handleChangeFile = jest.spyOn(newBill, 'handleChangeFile');
       const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
 
       const fileInput = screen.getByTestId('file');
@@ -111,18 +113,13 @@ describe('Given I am connected as an employee', () => {
       const file = new File(['(⌐□_□)'], 'chucknorris.pdf', { type: 'application/pdf' });
       await userEvent.upload(fileInput, file, { applyAccept: false });
       expect(handleChangeFile).toHaveReturnedWith(false);
-      /* await waitFor(() => expect(screen.findByText('Le fichier doit être au format jpg, jpeg ou png'))
-        .toBeTruthy());
-      !expect(fileInput.files.item(0)).toBe(file);
+      expect(screen.queryByTestId('error-message')).toBeVisible();
 
-       */
-
-
-      /* Submit the form by clicking on the button */
-      const form = screen.getByTestId('form-new-bill');
-      form.addEventListener('submit', handleSubmit);
-      userEvent.click(screen.getByTestId('btn-send-bill'));
+      const submit = screen.getByTestId('form-new-bill');
+      submit.addEventListener('submit', handleSubmit);
+      fireEvent.click(screen.getByTestId('btn-send-bill'));
       expect(handleSubmit).toHaveBeenCalled();
+
     });
 
     it('Should be able to send the form with a png file', async () => {
@@ -184,7 +181,6 @@ describe('Given I am connected as an employee', () => {
       const fileInput = screen.getByTestId("file");
       /* Add picture in the input */
       await userEvent.upload(fileInput, file, { applyAccept: false });
-
       expect(fileInput.files.item(0)).toBe(file);
       const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
 
@@ -194,155 +190,9 @@ describe('Given I am connected as an employee', () => {
       fireEvent.click(screen.getByTestId("btn-send-bill"));
 
       expect(handleSubmit).toHaveBeenCalled();
-      //expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"]);
       expect(screen.queryAllByText("Mes note de frais")).toBeTruthy();
     });
 
-    it('Should be able to send the form with a jpeg file', async () => {
-      document.body.innerHTML = "";
-      const html = NewBillUI();
-      document.body.innerHTML = html;
-
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-          email: "employee@test.tdl",
-        })
-      );
-
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage,
-      });
-
-      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
-      expect(screen.getByTestId("expense-type")).toBeTruthy();
-      expect(screen.getByTestId("expense-name")).toBeTruthy();
-      expect(screen.getByTestId("amount")).toBeTruthy();
-      expect(screen.getByTestId("datepicker")).toBeTruthy();
-      expect(screen.getByTestId("vat")).toBeTruthy();
-      expect(screen.getByTestId("pct")).toBeTruthy();
-      expect(screen.getByTestId("commentary")).toBeTruthy();
-      expect(screen.getByTestId("file")).toBeTruthy();
-      expect(screen.getByTestId("btn-send-bill")).toBeTruthy();
-      /* Complete the form */
-      const type = screen.getByTestId("expense-type");
-      const name = screen.getByTestId("expense-name");
-      const amount = screen.getByTestId("amount");
-      const date = screen.getByTestId("datepicker");
-      const vat = screen.getByTestId("vat");
-      const pct = screen.getByTestId("pct");
-      const commentary = screen.getByTestId("commentary");
-      userEvent.selectOptions(type, "Restaurants et bars");
-      userEvent.type(name, "Restaurant");
-      userEvent.type(amount, "100");
-      userEvent.type(date, "2021-10-10");
-      userEvent.type(vat, "20");
-      userEvent.type(pct, "20");
-      userEvent.type(commentary, "Restaurant");
-
-      const file = new File(["(⌐□_□)"], "chucknorris.jpeg", {
-        type: "image/jpeg",
-      });
-      const fileInput = screen.getByTestId("file");
-      /* Add picture in the input */
-      await userEvent.upload(fileInput, file, { applyAccept: false });
-
-      expect(fileInput.files.item(0)).toBe(file);
-      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
-
-      /* Submit the form by clicking on the button */
-      const form = screen.getByTestId("form-new-bill");
-      form.addEventListener("submit", handleSubmit);
-      fireEvent.click(screen.getByTestId("btn-send-bill"));
-
-      expect(handleSubmit).toHaveBeenCalled();
-      //expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"]);
-      expect(screen.queryAllByText("Mes note de frais")).toBeTruthy();
-    });
-
-    it('Should be able to send the form with a jpeg file', async () => {
-      document.body.innerHTML = "";
-      const html = NewBillUI();
-      document.body.innerHTML = html;
-
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-          email: "employee@test.tdl",
-        })
-      );
-
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage,
-      });
-
-      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
-      expect(screen.getByTestId("expense-type")).toBeTruthy();
-      expect(screen.getByTestId("expense-name")).toBeTruthy();
-      expect(screen.getByTestId("amount")).toBeTruthy();
-      expect(screen.getByTestId("datepicker")).toBeTruthy();
-      expect(screen.getByTestId("vat")).toBeTruthy();
-      expect(screen.getByTestId("pct")).toBeTruthy();
-      expect(screen.getByTestId("commentary")).toBeTruthy();
-      expect(screen.getByTestId("file")).toBeTruthy();
-      expect(screen.getByTestId("btn-send-bill")).toBeTruthy();
-      /* Complete the form */
-      const type = screen.getByTestId("expense-type");
-      const name = screen.getByTestId("expense-name");
-      const amount = screen.getByTestId("amount");
-      const date = screen.getByTestId("datepicker");
-      const vat = screen.getByTestId("vat");
-      const pct = screen.getByTestId("pct");
-      const commentary = screen.getByTestId("commentary");
-      userEvent.selectOptions(type, "Restaurants et bars");
-      userEvent.type(name, "Restaurant");
-      userEvent.type(amount, "100");
-      userEvent.type(date, "2021-10-10");
-      userEvent.type(vat, "20");
-      userEvent.type(pct, "20");
-      userEvent.type(commentary, "Restaurant");
-
-      const file = new File(["(⌐□_□)"], "chucknorris.jpg", {
-        type: "image/jpeg",
-      });
-      const fileInput = screen.getByTestId("file");
-      /* Add picture in the input */
-      await userEvent.upload(fileInput, file, { applyAccept: false });
-
-      expect(fileInput.files.item(0)).toBe(file);
-      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
-
-      /* Submit the form by clicking on the button */
-      const form = screen.getByTestId("form-new-bill");
-      form.addEventListener("submit", handleSubmit);
-      fireEvent.click(screen.getByTestId("btn-send-bill"));
-
-      expect(handleSubmit).toHaveBeenCalled();
-      //expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"]);
-      expect(screen.queryAllByText("Mes note de frais")).toBeTruthy();
-    });
     it('Should display an error if no file is sent', async () => {
       /* reset the html */
       document.body.innerHTML = '';
@@ -402,15 +252,16 @@ describe('Given I am connected as an employee', () => {
   });
 });
 
+
 describe('Test for addStatus function', () => {
   describe('If I send a type of status, I should get the corresponding DOM element', () => {
-    test("Should return a div with the ID 'error-message'", () => {
+    test(" Should return a div with the ID 'error-message'", () => {
       const status = 'error';
       const DOM = addStatus(status, 'test');
       expect(DOM).toBeTruthy();
       expect(DOM.id).toBe('error-message');
     });
-    test("Should return a div with the ID 'validation-message'", () => {
+    test(" Should return a div with the ID 'validation-message'", () => {
       const status = 'success';
       const DOM = addStatus(status, 'test');
       expect(DOM).toBeTruthy();
